6269b347bbf7233d99b3ad965ad3ed7fd939cbdd
Lucas Gomes Goulart <lucas.goulart@advicehealth.com.br>
2024-03-07 15:32:28 -0300

    template cypress

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
+  // video: true,
+  experimentalMemoryManagement: true,
+  viewportWidth: 1440,
+  viewportHeight: 854,
+  numTestsKeptInMemory: 1,
+  chromeWebSecurity: false,
+  on: {
+    before: (browser = {}, launchOptions) => {
+      console.log(launchOptions.args);
+
+      if (browser.name === 'chrome') {
+        launchOptions.args.push('--disable-gpu');
+      }
+
+      return launchOptions;
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+  },
+});
